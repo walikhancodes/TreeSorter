39ca873fec385a7bb4f75a4b4b8d095e28ed7756
Wali Khan <76940552+walikhancodes@users.noreply.github.com>
2021-01-04 15:39:10 -0800

    Update SelfBalancingTreeSet.java

--- SelfBalancingTreeSet.java
+++ SelfBalancingTreeSet.java
@@ -1,7 +1,4 @@
-/*THIS CODE WAS MY OWN WORK, 
-IT WAS WRITTEN WITHOUT CONSULTING ANYSOURCES 
-OUTSIDE OF THOSE APPROVED BY THE INSTRUCTOR. 
-[Wali Khan, 2308097]*/
+
 import java.util.Comparator;
 //** This will be my implementation of an AVL tree */
 public class SelfBalancingTreeSet<K> extends TreeSet<K> {
